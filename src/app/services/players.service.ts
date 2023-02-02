b7fc79f2011f85cad1500953182fbc2c2a72bc76
kanwar Dhillon <kdhillon469@gmail.com>
2023-02-02 12:04:12 +1100

    "bug fixes"

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+// import { environment } from 'src/environments/environment';
 import { PlayerModel } from '../models/player-model.model';
 // import { PlayerModel } from '../models/PlayerModel.model';
 
